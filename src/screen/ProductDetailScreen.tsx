f9c72c1a13fa904f021715d2b75f459fd03d85e7
Vu Quang <quangnv.0212@gmail.com>
2023-09-22 15:01:53 +0700

    profile screen

--- src/screen/ProductDetailScreen.tsx
+++ src/screen/ProductDetailScreen.tsx
@@ -1,12 +1,312 @@
-import { View, Text } from "react-native";
-import React from "react";
+import * as React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import { SIZES } from "../constants/sizes";
+import { useNavigation } from "@react-navigation/native";
+import {
+  Fontisto,
+  Ionicons,
+  MaterialCommunityIcons,
+  SimpleLineIcons,
+} from "@expo/vector-icons";
+import { COLORS } from "../constants/color";
+import productApi from "../apis/product.api";
+import { useQuery } from "@tanstack/react-query";
 
-const ProductDetailScreen = () => {
+interface ProductDetailScreenProps {
+  route?: any;
+  navigation?: any;
+}
+
+const ProductDetailScreen = ({
+  route,
+  navigation,
+}: ProductDetailScreenProps) => {
+  const id = route.params.id;
+  const [count, setCount] = React.useState(1);
+  const incrementCount = () => {
+    setCount((count) => count + 1);
+  };
+  const derementCount = () => {
+    setCount((count) => count - 1);
+  };
+  const addToCart = () => {};
+  const buyNow = () => {};
+  const {
+    data: productDetailData,
+    isError,
+    isLoading,
+  } = useQuery({
+    queryKey: ["product", id],
+    queryFn: () => productApi.getProductDetail(id as string),
+  });
+  const data = productDetailData?.data.data;
   return (
-    <View>
-      <Text>ProductDetailScreen</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator
+          style={{ margin: 30 }}
+          size={SIZES.xxLarge}
+          color={COLORS.primary}
+        />
+      ) : isError ? (
+        <Text
+          style={{
+            margin: 30,
+          }}
+        >
+          Something went wrong
+        </Text>
+      ) : (
+        <>
+          <View style={styles.upperRow}>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Ionicons name="chevron-back-circle" size={30} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => {}}>
+              <Ionicons name="heart" size={30} color={COLORS.primary} />
+            </TouchableOpacity>
+          </View>
+          <Image
+            source={{
+              uri: data?.image,
+            }}
+            style={styles.image}
+          />
+
+          <View style={styles.details}>
+            <View style={styles.titleRow}>
+              <Text style={styles.title}>{data?.name}</Text>
+              <View style={styles.priceWrapper}>
+                <Text style={styles.price}>
+                  {data?.price?.toLocaleString("vi", {
+                    style: "currency",
+                    currency: "VND",
+                  })}
+                </Text>
+              </View>
+            </View>
+
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+                gap: SIZES.large,
+                margin: SIZES.large,
+              }}
+            >
+              <View
+                style={{
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: 10,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    gap: 5,
+                  }}
+                >
+                  {[1, 2, 3, 4, 5].map((index) => (
+                    <Ionicons
+                      key={index}
+                      style={{ alignItems: "center" }}
+                      name="star"
+                      size={24}
+                      color="gold"
+                    />
+                  ))}
+                </View>
+                <Text style={{}}>4.9</Text>
+              </View>
+
+              <View style={styles.rating}>
+                <TouchableOpacity
+                  disabled={count == 1}
+                  onPress={() => derementCount()}
+                >
+                  <SimpleLineIcons name="minus" size={20} />
+                </TouchableOpacity>
+                <Text style={styles.ratingText}>{count}</Text>
+                <TouchableOpacity onPress={() => incrementCount()}>
+                  <SimpleLineIcons name="plus" size={20} />
+                </TouchableOpacity>
+              </View>
+            </View>
+
+            <View style={styles.descriptionWraper}>
+              <Text style={styles.description}>Description</Text>
+              <Text style={styles.descText}>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Repudiandae dignissimos ab veniam alias! Enim dolores error
+                consequuntur qui vel tenetur?
+              </Text>
+            </View>
+            <View style={{ marginBottom: SIZES.small }}>
+              <View style={styles.location}>
+                <View style={{ flexDirection: "row", alignItems: "center" }}>
+                  <Ionicons name="location-outline" size={20} />
+                  <Text>Hà Nội</Text>
+                </View>
+                <View
+                  style={{ flexDirection: "row", gap: 5, alignItems: "center" }}
+                >
+                  <MaterialCommunityIcons
+                    name="truck-delivery-outline"
+                    size={20}
+                  />
+                  <Text>Free Delivery</Text>
+                </View>
+              </View>
+            </View>
+            <View style={styles.cartRow}>
+              <TouchableOpacity onPress={buyNow} style={styles.cartBtn}>
+                <Text style={styles.cartTitle}>BUY NOW</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={addToCart} style={styles.addCart}>
+                <Fontisto
+                  name="shopping-bag"
+                  size={20}
+                  color={COLORS.lightWhite}
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        </>
+      )}
+    </ScrollView>
   );
 };
 
 export default ProductDetailScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  addCart: {
+    width: 37,
+    height: 37,
+    borderRadius: 50,
+    margin: SIZES.small,
+    backgroundColor: COLORS.black,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  description: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  descriptionWraper: {
+    marginHorizontal: SIZES.large,
+  },
+  descText: {
+    // fontFamily:"regular"
+    fontSize: SIZES.small,
+    textAlign: "justify",
+    marginBottom: SIZES.small,
+  },
+  title: {
+    // fontFamily: "bold",
+    fontSize: SIZES.large,
+  },
+  priceWrapper: {
+    backgroundColor: COLORS.secondary,
+    borderRadius: SIZES.large,
+    marginVertical: SIZES.medium,
+  },
+  price: {
+    paddingHorizontal: 10,
+    // fontFamily: "semibold",
+    fontSize: SIZES.large,
+  },
+  upperRow: {
+    marginHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    position: "absolute",
+    top: SIZES.xxLarge,
+    width: SIZES.width - 44,
+    zIndex: 999,
+  },
+  image: {
+    aspectRatio: 1,
+    resizeMode: "cover",
+  },
+  details: {
+    marginTop: -SIZES.large,
+    backgroundColor: COLORS.lightWhite,
+    width: SIZES.width,
+    borderTopLeftRadius: SIZES.medium,
+    borderTopRightRadius: SIZES.medium,
+  },
+  titleRow: {
+    marginHorizontal: 20,
+    paddingBottom: SIZES.small,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: SIZES.width - 44,
+    top: 20,
+  },
+  ratingRow: {
+    paddingBottom: SIZES.small,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: SIZES.width - 10,
+    top: 5,
+  },
+  rating: {
+    flexDirection: "row",
+    gap: 5,
+    justifyContent: "flex-start",
+    alignItems: "center",
+    marginHorizontal: SIZES.large,
+  },
+  ratingText: {
+    color: COLORS.gray,
+    justifyContent: "center",
+    // fontFamily:"medium"
+  },
+  location: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: COLORS.secondary,
+    marginHorizontal: 12,
+    padding: 5,
+    borderRadius: SIZES.large,
+  },
+  cartTitle: {
+    // fontFamily: "semibold",
+    fontSize: SIZES.medium,
+    color: COLORS.lightWhite,
+  },
+  cartRow: {
+    // paddingBottom: SIZES.small,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    // width: SIZES.width,
+  },
+  cartBtn: {
+    width: SIZES.width * 0.7,
+    backgroundColor: COLORS.black,
+    padding: SIZES.small / 2,
+    borderRadius: SIZES.large,
+    marginLeft: 12,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- src/screen/ProfileScreen.tsx
+++ src/screen/ProfileScreen.tsx
@@ -1,10 +1,228 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import * as React from "react";
+import {
+  Alert,
+  Image,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { COLORS } from "../constants/color";
+import { SIZES } from "../constants/sizes";
 
-export default function ProfileScreen() {
+interface ProfileScreenProps {}
+
+const ProfileScreen = (props: ProfileScreenProps) => {
+  const logout = () => {
+    Alert.alert("Logout", "Are you sure you want to log out", [
+      { text: "Cancel", onPress: () => console.log("cancel pressed") },
+      {
+        text: "Continue",
+        onPress: async () => {},
+      },
+    ]);
+  };
+  const navigation = useNavigation();
+  const userLogin = true;
+  const userdata = "";
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.container}>
+        <StatusBar backgroundColor={COLORS.gray} />
+        <View style={{ width: "100" }}>
+          <Image
+            style={styles.cover}
+            source={{
+              uri: `https://api-ecom.duthanhduoc.com/images/${userdata?.avatar}`,
+            }}
+          />
+        </View>
+        <View style={styles.profileContainer}>
+          <Image
+            style={styles.profile}
+            source={{
+              uri: `https://api-ecom.duthanhduoc.com/images/${userdata?.avatar}`,
+            }}
+          />
+          <Text style={styles.name}>
+            {userLogin ? "Vu Quang" : "Please login to your account"}
+          </Text>
+          {!userLogin ? (
+            <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+              <View style={styles.loginBtn}>
+                <Text>Login</Text>
+              </View>
+            </TouchableOpacity>
+          ) : (
+            <View style={styles.loginBtn}>
+              <Text>{userdata?.email}</Text>
+            </View>
+          )}
+          {!userLogin ? (
+            <View></View>
+          ) : (
+            <ScrollView>
+              <View style={styles.menuWrapper}>
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate("Cart");
+                  }}
+                >
+                  <View style={menuItem(0.2)}>
+                    <MaterialCommunityIcons
+                      name="cart-outline"
+                      size={24}
+                      color={COLORS.primary}
+                    />
+                    <Text style={styles.menuText}>Cart</Text>
+                  </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  onPress={() => {
+                    navigation.navigate("EditProfile");
+                  }}
+                >
+                  <View style={menuItem(0.2)}>
+                    <MaterialCommunityIcons
+                      name="logout"
+                      size={24}
+                      color={COLORS.primary}
+                    />
+                    <Text style={styles.menuText}>Edit Profile</Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => {}}>
+                  <View style={menuItem(0.2)}>
+                    <FontAwesome
+                      name="language"
+                      size={24}
+                      color={COLORS.primary}
+                    />
+                    <Text style={styles.menuText}>Language</Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={logout}>
+                  <View style={menuItem(0.2)}>
+                    <MaterialCommunityIcons
+                      name="logout"
+                      size={24}
+                      color={COLORS.primary}
+                    />
+                    <Text style={styles.menuText}>Log out</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            </ScrollView>
+          )}
+        </View>
+      </View>
     </View>
   );
-}
+};
+
+export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightWhite,
+  },
+  cover: {
+    height: 290,
+    width: "100%",
+    resizeMode: "cover",
+  },
+  profileContainer: {
+    flex: 1,
+    alignItems: "center",
+  },
+  profile: {
+    height: 155,
+    width: 155,
+    borderRadius: 999,
+    borderColor: COLORS.primary,
+    borderWidth: 2,
+    resizeMode: "cover",
+    marginTop: -90,
+  },
+  name: {
+    // fontFamily: "bold",
+    color: COLORS.primary,
+    marginVertical: 5,
+  },
+  loginBtn: {
+    backgroundColor: COLORS.secondary,
+    padding: 10,
+    borderWidth: 0.4,
+    borderColor: COLORS.primary,
+    borderRadius: SIZES.xxLarge,
+  },
+  menuText: {
+    // fontFamily: "regular",
+    color: COLORS.gray,
+    // marginLeft: 20,
+    fontWeight: "600",
+    fontSize: 14,
+  },
+  menuWrapper: {
+    marginTop: SIZES.xLarge,
+    width: SIZES.width - SIZES.large,
+    backgroundColor: COLORS.lightWhite,
+    borderRadius: 12,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: "#F194FF",
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+  },
+});
+
+const menuItem = (borderBottomWidth: number) => ({
+  borderBottomWidth: borderBottomWidth,
+  flexDirection: "row",
+  paddingVertical: 15,
+  paddingHorizontal: 30,
+  borderColor: COLORS.gray,
+  gap: SIZES.medium,
+});
